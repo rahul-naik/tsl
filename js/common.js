b5bbd1ceeabe2b954e04db82dc32750eb33e18bc
Rahul Naik <rahul.naik@cognizant.com>
2016-11-24 16:00:34 +0530

    Index page

--- js/common.js
+++ js/common.js
@@ -1,17 +1,49 @@
 $(function () {
     /***side bar expand collapse code */
     var $sidebarEventTarget = $('#collapseSideBar');
-    var $hoverContainer = $('#colapseOnHover');
+    var $hoverContainer = $('#left-sidebar-nav');
     var $sidebarContainer = $('#left-sidebar-nav');
+
     $sidebarEventTarget.on('click', function (e) {
         e.stopPropagation();
         e.preventDefault();
-        $sidebarContainer.toggleClass('sidebar-collapsed');
-        $('.main-container').toggleClass('shift-container');
+        if ($sidebarContainer.hasClass('sidebar-collapsed')) {
+            $sidebarContainer.removeClass('sidebar-collapsed');
+            $('.main-container').removeClass('shift-container');
+
+            if ($(this).is(':hidden')) {
+                $(this).show(function () {
+                    $hoverContainer.removeClass('hoverMe');
+                    $hoverContainer.removeClass('display-over');
+                });
+            } else {
+                $(this).show(function () {
+                    $hoverContainer.removeClass('hoverMe');
+                    $hoverContainer.removeClass('display-over');
+                });
+            }
+
+        } else {
+            $sidebarContainer.addClass('sidebar-collapsed');
+            $('.main-container').addClass('shift-container');
+           
+                $(this).hide(function () {
+                    $hoverContainer.addClass('hoverMe');
+                });
+            
+        }
+
+
+
     });
 
-    $hoverContainer.on('mouseover',function(){
-        $sidebarContainer.removeClass('sidebar-collapsed');
+    $(document).on('mouseover', '.hoverMe', function () {
+        $sidebarContainer.addClass('display-over');
+        $sidebarEventTarget.show();
+    });
+    $(document).on('mouseleave', '.hoverMe', function () {
+        $sidebarContainer.removeClass('display-over');
+        $sidebarEventTarget.hide();
     });
 
 });
